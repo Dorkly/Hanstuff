445e1527ab60266ec61df3f0cdde2ccedb777521
Jim Helm <jhelm6@gatech.edu>
2018-02-14 14:19:26 -0500

    changes
    changes

--- DjangoWebProject2/app/views.py
+++ DjangoWebProject2/app/views.py
@@ -14,7 +14,7 @@ def home(request):
         request,
         'app/index.html',
         {
-            'title':'Home Page',
+            'title':'Hanstuff',
             'year':datetime.now().year,
         }
     )
---
+++ DjangoWebProject2/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jhelm\source\repos\Hanstuff\Hanstuff\hanenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
