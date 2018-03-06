45c8c64f22ce80d5997a041fe7251a6893fce003
Dorkly <bree_cakesx3@live.com>
2018-03-05 23:53:37 -0500

    integrating justhost, github and java together in intellij

--- DjangoWebProject2/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\jhelm\source\repos\DjangoWebProject2\DjangoWebProject2\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
