310d3b1a9fd24855167f6a638cb8eace84330879
Austin Jiang <huajiang@gatech.edu>
2018-04-15 23:13:20 -0400

    user comments

--- HTML/intermediate.php
+++ HTML/intermediate.php
@@ -77,6 +77,11 @@ Session_start();
     <div class ="col-sm-6=8">
         <iframe src="images/pdfread.pdf" width ="100%" height ="500px" scrolling ="yes">
         </iframe></div>
+    <!-- begin wwww.htmlcommentbox.com -->
+    <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">HTML Comment Box</a> is loading comments...</div>
+    <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+    <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24CZFwVYp.l7lLTcpLFv4AJ0"+"&opts=16863&num=10&ts=1523649694696");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+    <!-- end www.htmlcommentbox.com -->
 
 
 </div>
