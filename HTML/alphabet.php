58cd7bd0d58661e99d2ad99021abb80d639c4541
Dorkly <bree_cakesx3@live.com>
2018-04-20 17:08:15 -0400

    updating alphabet page

--- HTML/alphabet.php
+++ HTML/alphabet.php
@@ -6,10 +6,14 @@
 <?php include ("header.php"); ?>
 
 <div class="jumbotron" style = " background: url(../images/lamps.jpg) no-repeat center; background-size: 100% 100%;">
-    Alphabet
-    Let's learn the Korean alphabet!
+    <h1 style = "text-align: center">Alphabet</h1>
+    <h3 style = "text-align: center">Let's learn the Korean alphabet!</h3>
 </div>
 
+<div class ="col-sm-8">
+    <iframe src="images/Beginner_Prelim01_05.pdf" width ="150%" height ="500px" scrolling ="yes">
+    </iframe>
+</div>
 
 
 <?php include ("footer.php"); ?>
